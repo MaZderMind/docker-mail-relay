5d561123ccfb7a2566d13230956039e89da0eb7f
Peter Körner <pkoerner@seibert-media.net>
2017-08-23 19:59:08 +0200

    configurable sender-rewrite

--- run.sh
+++ run.sh
@@ -13,6 +13,7 @@ export RELAY_HOST_NAME=${RELAY_HOST_NAME:-"relay.example.com"}
 export ACCEPTED_NETWORKS=${ACCEPTED_NETWORKS:-"192.168.0.0/16 172.16.0.0/12 10.0.0.0/8"}
 export USE_TLS=${USE_TLS:-"no"}
 export TLS_VERIFY=${TLS_VERIFY:-"may"}
+export RELAY_FORCE_SENDER=${RELAY_FORCE_SENDER:-""}
 
 echo $RELAY_HOST_NAME > /etc/mailname
 
@@ -20,6 +21,11 @@ echo $RELAY_HOST_NAME > /etc/mailname
 j2 /root/conf/postfix-main.cf > /etc/postfix/main.cf
 j2 /root/conf/sasl_passwd | tr -d '\n' > /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
+
+if [ -n "$RELAY_FORCE_SENDER" ] ; then
+	j2 /root/conf/sender_canonical_maps > /etc/postfix/sender_canonical_maps
+	j2 /root/conf/header_check > /etc/postfix/header_check
+fi
 
 # Launch
 rm -f /var/spool/postfix/pid/*.pid
